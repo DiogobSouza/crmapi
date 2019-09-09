ed864f3f1018ebc3580ee2f2890c55d957963f17
Diogo <Diogo@DESKTOP-N12VNVT>
2019-09-09 11:36:25 -0300

    Rota
    rota

--- app.py
+++ app.py
@@ -7,6 +7,9 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World!'
 
+@app.route('/deploy', methods=['GET'])
+def deploy():
+    return "<h1> Testando deploy </h1>"
 
 if __name__ == '__main__':
     app.run()
