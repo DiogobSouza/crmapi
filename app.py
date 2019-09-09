20cefe8d514b757153a74f40ca36768233c645f4
Diogo <Diogo@DESKTOP-N12VNVT>
2019-09-09 11:43:48 -0300

    mudanças da rota

--- app.py
+++ app.py
@@ -5,11 +5,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
-
-@app.route('/deploy', methods=['GET'])
-def deploy():
     return "<h1> Testando deploy </h1>"
+
+
 
 if __name__ == '__main__':
     app.run()
